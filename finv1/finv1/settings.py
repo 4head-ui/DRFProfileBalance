a08ed2be0d952b5449ec1caad10d7ffd084d7263
Pavel <106407898+4head-ui@users.noreply.github.com>
2023-01-16 22:02:05 +0300

    Update settings.py

--- finv1/finv1/settings.py
+++ finv1/finv1/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = '*************'
 EMAIL_HOST_USER = '**************'
 EMAIL_HOST_PASSWORD = '*************'
 EMAIL_PORT = 587
