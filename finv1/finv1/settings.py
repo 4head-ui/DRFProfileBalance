2a55d09f8911bf82e686eb3c280835dac09658c9
Pavel <106407898+4head-ui@users.noreply.github.com>
2023-02-09 16:17:09 +0300

    Update settings.py

--- finv1/finv1/settings.py
+++ finv1/finv1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)g+g5qj!t#h#ekz(mu&zua1=d0ke1&vcg53n3j+p_g-!i&1p^=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
